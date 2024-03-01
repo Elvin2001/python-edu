c2e5a16347bd21b8e851edd283dc3af725044ade
Elvin Abaev <elvinabaev@gmail.com>
2024-03-01 13:20:18 +0300

    Lesson 3. Meeting with lists and conditions.

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-name = input("Здравствуйте, введите пожалуйста имя:\n")
-print(f"{name}, вы вошли в систему, спасибо за доверие!")
+shopping_list = ["Яблоки", "Молоко"]
+destination = input("Куда пойдёте, Сударь?\n")
 
+if destination.lower() == "пикник":
+    print("Надо не забыть купить мясо и дрова!")
+    shopping_list.extend(["Мясо", "Дрова"])
+
+elif destination.lower() == "гости":
+    print("Надо не забыть купить торт и вино!")
+    shopping_list.extend(["Торт", "Вино"])
+
+else:
+    print("Хорошо, что никуда сегодня не идём... нужно что-то вкусненькое купить!")
+    shopping_list.extend(["Мороженое", "Попкорн"])
+
+print(f"Всего товаров в корзине - {len(shopping_list)}\n, ваш список товаров: [{shopping_list}].")
